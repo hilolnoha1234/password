29818fcdb32a518c2f0bcf967b4a98d30ff76307
hilolnoha1234 <152533843+hilolnoha1234@users.noreply.github.com>
2023-12-07 20:40:10 +0100

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,10 @@
+from random import choice
+
+symbol = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+len_password = int(input("how long would you like your password to be? enter in numbers"))
+password = ""
+
+for i in range(len_password):
+    password += choice(symbol)
+
+print(password)
